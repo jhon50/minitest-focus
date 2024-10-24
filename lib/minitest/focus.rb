ebe20cd6e50bda19e5eed50afc1a070dbb45aafa
Jhonatan Teixeira <johnn_lds@hotmail.com>
2024-10-24 16:18:07 +0100

    Allow focus on describe block

--- lib/minitest/focus.rb
+++ lib/minitest/focus.rb
@@ -1,14 +1,14 @@
 require "minitest/test"
 
-class Minitest::Test    # :nodoc:
-  class Focus           # :nodoc:
-    VERSION = "1.4.0"   # :nodoc:
+class Minitest::Test # :nodoc:
+  class Focus # :nodoc:
+    VERSION = "1.4.0" # :nodoc:
   end
 
   @@filtered_names = [] # :nodoc:
 
-  def self.add_to_filter name
-    @@filtered_names << "#{self}##{name}"
+  def self.add_to_filter name, object = self
+    @@filtered_names << "#{object}##{name}"
   end
 
   def self.filtered_names
@@ -16,7 +16,7 @@ class Minitest::Test    # :nodoc:
   end
 
   ##
-  # Focus on the next test defined. Cumulative. Equivalent to
+  # Focus on the next test/describe defined. Cumulative. Equivalent to
   # running with command line arg: -n /test_name|.../
   #
   #   class MyTest < Minitest::Test
@@ -38,7 +38,15 @@ class Minitest::Test    # :nodoc:
   #   end
 
   def self.focus name = nil
-    if name then
+    if name.is_a?(Class) # describe inline
+      name.methods_matching(/^test_/).each do |test|
+        add_to_filter test, name
+      end
+      name.children.each do |child|
+        # nesting
+        focus child
+      end
+    elsif name
       add_to_filter name
     else
       set_focus_trap
@@ -46,14 +54,30 @@ class Minitest::Test    # :nodoc:
   end
 
   ##
-  # Sets a one-off method_added callback to set focus on the method
-  # defined next.
+  # Sets a one-off method_added callback to set focus on the
+  # method/describe defined next.
 
   def self.set_focus_trap
-    meta = class << self; self; end
+    meta =
+
+      class << self;
+        self;
+      end
 
     meta.send :define_method, :method_added do |name|
       add_to_filter name
+
+      meta.send :remove_method, :method_added
+      meta.send :remove_method, :describe
+    end
+
+    meta.send :define_method, :describe do |name, &block|
+      meta.send :remove_method, :describe
+      meta.send :define_method, :method_added do |name|
+        add_to_filter name
+      end
+
+      super name, &block
 
       meta.send :remove_method, :method_added
     end
--- test/minitest/test_focus.rb
+++ test/minitest/test_focus.rb
@@ -2,18 +2,68 @@ require "minitest/autorun"
 require "minitest/focus"
 
 class MyTest1 < Minitest::Test
-         def test_fail;            flunk; end
-  focus; def test_method;          pass;  end
-  focus  def test_method2;         pass;  end
-         focus \
-         def test_method3;         pass;  end
-         def test_method_edgecase; flunk; end
+  def test_fail
+    flunk;
+  end
+
+  focus;
+
+  def test_method
+    pass;
+  end
+
+  focus def test_method2
+    pass;
+  end
+  focus \
+    def test_method3
+      pass;
+    end
+
+  def test_method_edgecase
+    flunk;
+  end
 end
 
 describe "MyTest2" do
-         it "is ignored"            do flunk end
-  focus; it "does something"        do pass  end
-  focus  it("does something else")  { pass } # stupid block precedence needs {}
-         it "bombs"                 do flunk end
-  focus; it "has non-word ['chars'" do pass  end # Will raise invalid RegExp unless correctly escaped
+  it "is ignored" do
+    flunk
+  end
+  focus; it "does something" do
+    pass
+  end
+  focus it("does something else") { pass } # stupid block precedence needs {}
+  it "bombs" do
+    flunk
+  end
+  focus; it "has non-word ['chars'" do
+    pass
+  end # Will raise invalid RegExp unless correctly escaped
+end
+
+describe "MyTest3" do
+  describe("Nope") { it "nope" do
+    flunk
+  end }
+  focus describe("Inline") { it "inline" do
+    pass
+  end
+  describe("Inline^2") { it "inline^2" do
+    pass
+  end } }
+  describe("Middle") { it "middle" do
+    flunk
+  end }
+  focus; describe("Next") { it "next" do
+    pass
+  end
+  describe("Next^2") { it "next^2" do
+    pass
+  end } }
+  describe("Trail") { it "trail" do
+    flunk
+  end }
+  focus; describe("Empty") {}; it "post empty" do
+    flunk
+  end
 end
